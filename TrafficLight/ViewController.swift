d77845a1a92e8dcfa984d5a61f5c2c1e8f8cc57a
Iurii <Iurii>
2023-04-23 00:09:47 +0500

    Build Interface

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
+    private var cornerRadius: CGFloat = 65
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        redView.layer.cornerRadius = cornerRadius
+        yellowView.layer.cornerRadius = cornerRadius
+        greenView.layer.cornerRadius = cornerRadius
+        switchButton.layer.cornerRadius = cornerRadius - 55
     }
 
-   
+    @IBAction func switchButtonTapped() {
+        switchButton.setTitle("NEXT", for: .normal)
+        
+    }
 }
 
