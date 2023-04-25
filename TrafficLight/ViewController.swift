2dd597ab0cd531dbb6d8a22a7e0521d6bfd8cf87
Iurii <Iurii>
2023-04-25 21:17:51 +0500

    Work after feedback

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,49 +9,50 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    @IBOutlet var redView: UIView!
-    @IBOutlet var yellowView: UIView!
-    @IBOutlet var greenView: UIView!
-    @IBOutlet var switchButton: UIButton!
-    private var cornerRadius: CGFloat = 65
-    
     enum PossibleLight {
         case red
         case yellow
         case green
     }
     
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     private var currentLight = PossibleLight.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redView.layer.cornerRadius = cornerRadius
-        yellowView.layer.cornerRadius = cornerRadius
-        greenView.layer.cornerRadius = cornerRadius
-        switchButton.layer.cornerRadius = cornerRadius - 55
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        switchButton.layer.cornerRadius = 10
     }
     
     @IBAction func switchButtonTapped() {
-        switchButton.setTitle("NEXT", for: .normal)
-        
-        
-        func changeAlpfa(for possibleLight: PossibleLight) {
-            switch possibleLight {
-            case .red:
-                greenView.alpha = 0.3
-                redView.alpha = 1
-                currentLight = PossibleLight.yellow
-            case .yellow:
-                redView.alpha = 0.3
-                yellowView.alpha = 1
-                currentLight = PossibleLight.green
-            case .green:
-                yellowView.alpha = 0.3
-                greenView.alpha = 1
-                currentLight = PossibleLight.red
-            }
+        if switchButton.currentTitle == "START" {
+            switchButton.setTitle("NEXT", for: .normal)
         }
+        
         changeAlpfa(for: currentLight)
+    }
+    
+    private func changeAlpfa(for possibleLight: PossibleLight) {
+        switch possibleLight {
+        case .red:
+            greenView.alpha = 0.3
+            redView.alpha = 1
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            currentLight = .green
+        case .green:
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            currentLight = .red
+        }
     }
 }
 
